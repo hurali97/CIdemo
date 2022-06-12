534f12ac673fdadf2b470b6e9869cf8e2466db46
Hur Ali <hurali97@gmail.com>
2022-06-12 16:21:31 +0500

    tested e2e for App.tsx on ios

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,5 +1,8 @@
+const detox = require('detox');
+
 describe('CIdemo', () => {
     beforeAll(async () => {
+        // await detox.init();
         await device.launchApp();
     });
 
@@ -7,8 +10,16 @@ describe('CIdemo', () => {
         await device.reloadReactNative();
     });
 
+    it('should have "Step One" section', async () => {
+        await expect(element(by.text('Step One'))).toBeVisible();
+    });
+
+    it('should have "See Your Changes" section', async () => {
+        await expect(element(by.text('See Your Changes'))).toBeVisible();
+    });
+
     it('should have View', async () => {
-        await expect(element(by.id('MyUniqueId123'))).toBeVisible();
+        await expect(element(by.id('MyUniqueId123'))).toExist();
     });
 
     // it('should show hello screen after tap', async () => {
