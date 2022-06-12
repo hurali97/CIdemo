eb00fe4c31c99c810bc5bd85eb43c512738dee81
Muhammad Hur Ali <hurali97@gmail.com>
2022-06-12 18:10:11 +0500

    e2e setup (#6)
    * initial files from detox init
    
    * updated configurations to run first test
    
    * tested e2e for App.tsx on ios
    
    * changed e2e tests to TS
    
    * added transformer to e2e/config.json

---
+++ e2e/firstTest.e2e.ts
@@ -0,0 +1,33 @@
+import { expect } from 'detox';
+
+describe('CIdemo', () => {
+    beforeAll(async () => {
+        await device.launchApp();
+    });
+
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it('should have "Step One" section', async () => {
+        await expect(element(by.text('Step One'))).toBeVisible();
+    });
+
+    it('should have "See Your Changes" section', async () => {
+        await expect(element(by.text('See Your Changes'))).toBeVisible();
+    });
+
+    it('should have View', async () => {
+        await expect(element(by.id('MyUniqueId123'))).toExist();
+    });
+
+    // it('should show hello screen after tap', async () => {
+    //   await element(by.id('hello_button')).tap();
+    //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+    // });
+
+    // it('should show world screen after tap', async () => {
+    //   await element(by.id('world_button')).tap();
+    //   await expect(element(by.text('World!!!'))).toBeVisible();
+    // });
+});
