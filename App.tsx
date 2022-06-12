64f8d063c96d270cbe5e5e3f6d626f3ac2871f0d
Hur Ali <hurali97@gmail.com>
2022-06-12 20:16:56 +0500

    removed inline styles from App.tsx

--- App.tsx
+++ App.tsx
@@ -9,14 +9,19 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import {
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    useColorScheme,
+} from 'react-native';
 import Navigation from './src/navigation';
 
 const App = () => {
     const isDarkMode = useColorScheme() === 'dark';
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={styles.safeArea}>
             <StatusBar
                 barStyle={isDarkMode ? 'light-content' : 'dark-content'}
             />
@@ -26,3 +31,7 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+    safeArea: { flex: 1 },
+});
